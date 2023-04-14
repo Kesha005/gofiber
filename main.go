201af48ef82e2f205d23873b6967cbc202d65cf8
Kesha005 <backendtm005@gmail.com>
2023-04-14 15:28:01 +0500

    api_with_validation

--- main.go
+++ main.go
@@ -3,32 +3,34 @@ package main
 import "github.com/gin-gonic/gin"
 
 
+
 func main(){
 	app := gin.Default()
 
 	app.GET("/", index)
-	app.GET("/about",about)
 	app.GET("/contact",contact)
+	app.GET("/about",about)
 
 	app.Run()
+
 }
 
 
 func index(c *gin.Context){
 	c.JSON(200,gin.H{
-		"index": "index page",
+		"index":"Index page",
 	})
 }
-
-func about(c *gin.Context){
-	c.JSON(200,gin.H{
-		"about":"About page",
-	})
-}
-
 
 func contact(c *gin.Context){
 	c.JSON(200,gin.H{
-		"contact": "Contact page",
+		"contact":"contact_page",
+	})
+}
+
+
+func about(c *gin.Context){
+	c.JSON(200,gin.H{
+		"about":"about page",
 	})
 }
